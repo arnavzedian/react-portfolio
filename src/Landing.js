dff9fbfe1a7443e5d6f66cf91784842c5fa5177a
Arnav Singh <36072753+itsarnavsingh@users.noreply.github.com>
2023-07-09 11:51:32 +0530

    typo fix

--- src/Landing.js
+++ src/Landing.js
@@ -26,7 +26,7 @@ When I was in 12th, I started freelancing, I ended up gaining a lot of knowledge
 
 I joined LPU in summer of 2020. At the end of the semester, I realized I have mistaken semester fees for annual fees but rather than telling Papa, I tried earning that 80K rupees by myself, and to my surprise I succeeded. Well not technically, at the time it used to take 1 week for money to be released from Upwork. So, I had to borrow 30K from my mother, which I returned later. I also paid the hostel fees for the the 3rd year.  <br/> <br/>
 
-During this period, I learned about React (even tried creating my own React library), learned flutter, laravel & started work on a open source social network and created a few games.`;
+During this period, I learned about React (even tried creating my own React library), learned flutter, laravel & started work on a various personal projects.`;
 
 const Container = styled.div`
   font-size: 21px;
